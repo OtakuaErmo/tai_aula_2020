b4ce555446163ac9ec690e096354daa4390cb036
Jackson <lordjackson@gmail.com>
2020-03-31 10:57:22 -0300

    Configurando PDO para conexão com o banco de dados

--- BD.php
+++ BD.php
@@ -5,18 +5,21 @@ class BD
 
     public function connection()
     {
+        $bd_tipo = "mysql";
         $host = "localhost";
-        $db_nome = "db_tai_aula_2020";
-        $db_usuario = "root";
-        $db_senha = "123456";
-        $db_charset = "utf8";
-        $str_conn = "mysql:host=" . $host . ";dbname=" . $db_nome;
+        $bd_nome = "bd_tai_aula_2020";
+        $bd_porta = "3306";
+        $bd_usuario = "root";
+        $bd_senha = "";
+        $bd_charset = "utf8";
+
+        $str_conn = $bd_tipo . ":host=" . $host . ";dbname=" . $bd_nome . ";port=" . $bd_porta;;
 
         return new PDO(
             $str_conn,
-            $db_usuario,
-            $db_senha,
-            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES " . $db_charset)
+            $bd_usuario,
+            $bd_senha,
+            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES " . $bd_charset)
         );
     }
 
@@ -30,35 +33,10 @@ class BD
         return $stmt;
     }
 
-    public function insert($dados){
-        
-        $sql = "INSERT INTO cliente (nome, telefone, cpf, e-mail) 
-            VALUES (?, ?, ?, ?);";
-
-        $conn = $this->connection();
-        $stmt = $conn->prepare($sql);
-        $stmt->execute([ $dados['nome'],
-            $dados['telefone'],$dados['cpf'],$dados['e-mail'] ]);
-
-        return $stmt;
-
-    }
 }
-
-$dados = array(
-    "nome" => "João",
-    "telefone" => "84 98855-5500",
-    "cpf" => "55500055588",
-    "e-mail"=> "lordjoao@gmail.com"
-);
 
 $obj = new BD;
 $obj->connection();
 
 $result = $obj->selectAll();
 
-$obj->insert($dados);
-
-foreach ($result as $item) {
-    echo $item['id'] . "-" . $item['nome'] . "<br>";
-}
