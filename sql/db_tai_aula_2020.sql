01e7b44bcf04a36ba3e63c540fdca0228f2859e0
Jackson Meires <lordjackson@gmail.com>
2020-04-05 15:55:01 -0300

    Conexao BD - Editando e Listando - Ajustes, add botão voltar

--- sql/db_tai_aula_2020.sql
+++ sql/db_tai_aula_2020.sql
@@ -16,13 +16,13 @@ CREATE TABLE IF NOT EXISTS `cliente` (
   `nome` varchar(100) COLLATE utf8_bin DEFAULT NULL,
   `telefone` varchar(20) COLLATE utf8_bin DEFAULT NULL,
   `cpf` varchar(14) COLLATE utf8_bin DEFAULT NULL,
-  `e-mail` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- Copiando dados para a tabela db_tai_aula.cliente: 2 rows
 /*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
-INSERT INTO `cliente` (`id`, `nome`, `telefone`, `cpf`, `e-mail`) VALUES
+INSERT INTO `cliente` (`id`, `nome`, `telefone`, `cpf`, `email`) VALUES
 	(1, 'Jackson', '84 8800-5500', '555.000.555-11', 'exemplo@gmail.com'),
 	(2, 'Maria', '84 9900-5500', '000.555.000-55', 'exemplo2@gmail.com');
 /*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
