ebc80b24de14ab98380020393fedaaecd54e7da0
Jackson Meires <lordjackson@gmail.com>
2020-04-24 09:56:18 -0300

    Conexao BD - Editando e Listando

--- sql/db_tai_aula_2020.sql
+++ sql/db_tai_aula_2020.sql
@@ -1,94 +1,60 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Apr 24, 2020 at 12:38 PM
--- Server version: 5.7.24
--- PHP Version: 7.2.27
-
---
--- Database: `db_tai_aula_2020`
---
-
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           5.7.24 - MySQL Community Server (GPL)
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              11.0.0.5919
 -- --------------------------------------------------------
 
---
--- Table structure for table `cliente`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-CREATE TABLE `cliente` (
-  `id` int(11) NOT NULL,
+
+-- Copiando estrutura do banco de dados para db_tai_aula_2020
+CREATE DATABASE IF NOT EXISTS `db_tai_aula_2020` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+USE `db_tai_aula_2020`;
+
+-- Copiando estrutura para tabela db_tai_aula_2020.cliente
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) COLLATE utf8_bin DEFAULT NULL,
   `telefone` varchar(20) COLLATE utf8_bin DEFAULT NULL,
   `cpf` varchar(14) COLLATE utf8_bin DEFAULT NULL,
   `email` varchar(100) COLLATE utf8_bin DEFAULT NULL,
-  `municipio_id` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `municipio_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_cliente_municipio` (`municipio_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=51 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Dumping data for table `cliente`
---
-
+-- Copiando dados para a tabela db_tai_aula_2020.cliente: 5 rows
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
 INSERT INTO `cliente` (`id`, `nome`, `telefone`, `cpf`, `email`, `municipio_id`) VALUES
-(1, 'Jackson', '84 8800-5500', '555.000.555-11', 'exemplo@gmail.com', 2),
-(2, 'Maria', '84 9900-5500', '000.555.000-55', 'exemplo2@gmail.com', 1),
-(3, 'Luiz Macedo', '49 8855-1055', '44455501099', 'luizmacedo@gmail.com', 3),
-(4, 'João Macedo', '84 98855-5501', '55501055588', 'lordjoao1@gmail.com', 1),
-(37, 'Joãozinho dos Teclados', '49 88998899', '000.555.444-99', 'joaozinhodosteclados@gmail.com', 3);
+	(1, 'Jackson', '84 8800-5500', '555.000.555-11', 'exemplo@gmail.com', 2),
+	(2, 'Maria', '84 9900-5500', '000.555.000-55', 'exemplo2@gmail.com', 1),
+	(3, 'Luiz Macedo', '49 8855-1055', '44455501099', 'luizmacedo@gmail.com', 3),
+	(4, 'João Macedo', '84 98855-5501', '55501055588', 'lordjoao1@gmail.com', 1),
+	(37, 'Joãozinho dos Teclados', '49 88998899', '000.555.444-99', 'joaozinhodosteclados@gmail.com', 3);
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `municipio`
---
-
-CREATE TABLE `municipio` (
-  `id` int(11) NOT NULL,
+-- Copiando estrutura para tabela db_tai_aula_2020.municipio
+CREATE TABLE IF NOT EXISTS `municipio` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(150) COLLATE utf8_bin NOT NULL,
   `uf` varchar(2) COLLATE utf8_bin NOT NULL,
-  `estado` varchar(100) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `estado` varchar(100) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Dumping data for table `municipio`
---
-
+-- Copiando dados para a tabela db_tai_aula_2020.municipio: ~2 rows (aproximadamente)
+/*!40000 ALTER TABLE `municipio` DISABLE KEYS */;
 INSERT INTO `municipio` (`id`, `nome`, `uf`, `estado`) VALUES
-(1, 'Xanxerê', 'SC', 'Santa Catarina'),
-(2, 'Natal', 'RN', 'Rio Grande do Norte'),
-(3, 'Chapecó', 'SC', 'Santa Catarina');
+	(1, 'Xanxerê', 'SC', 'Santa Catarina'),
+	(2, 'Natal', 'RN', 'Rio Grande do Norte'),
+	(3, 'Chapecó', 'SC', 'Santa Catarina');
+/*!40000 ALTER TABLE `municipio` ENABLE KEYS */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `cliente`
---
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_cliente_municipio` (`municipio_id`);
-
---
--- Indexes for table `municipio`
---
-ALTER TABLE `municipio`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `cliente`
---
-ALTER TABLE `cliente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
-
---
--- AUTO_INCREMENT for table `municipio`
---
-ALTER TABLE `municipio`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-COMMIT;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
