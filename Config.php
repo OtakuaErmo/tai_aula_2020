c891eeff5d4308301ae52142cfff27ed7e88a90a
Jackson Meires <lordjackson@gmail.com>
2020-05-29 09:26:14 -0300

    Refatorando Projeto para MVC - class BD para Model

---
+++ Config.php
@@ -0,0 +1,12 @@
+<?php
+class Config
+{
+    //variaveis para configurar a conexao com o banco de dados
+    const BD_TIPO = "mysql";
+    const BD_HOST = "localhost";
+    const BD_NOME = "db_tai_aula_2020";
+    const BD_PORTA = "3306";
+    const BD_USUARIO = "root";
+    const BD_SENHA = "123456";
+    const BD_CHARSET = "utf8mb4";
+}
